c6e31436be33e5959b25d26ce3640afd3901368c
nhendy <nhendy@purdue.edu>
2019-04-16 01:18:58 -0400

    reorganize

--- RPi/Detection2/videos/rename_img.py
+++
@@ -1,6 +0,0 @@
-path = "D:/caltech/"
-import os
-for filename in os.listdir(path):
-    print(filename)
-    # newname = "airplane_" + filename
-    # os.rename(os.path.join(path,filename),os.path.join(path,newname))
