91c519f92ba823818d7fb9ee12902a2bf01af3f9
taniarini <tchakrab@purdue.edu>
2019-04-21 21:15:03 -0400

    please let this be the last time i fix this

--- App/app/__init__.py
+++ App/app/__init__.py
@@ -1,14 +1,61 @@
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import os
-
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_security import Security, SQLAlchemyUserDatastore, utils
+from app.admin.view import AdminView
+from flask_user import UserManager
+import datetime
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+login.login_view = 'auth.login'
 
-from app import routes
+
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
+
+
+from app.models import User, Role
+# user_manager = UserManager(app, db, User)
+# user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+# security = Security(app, user_datastore)
+
+
+admin = Admin(app, name='parkme', template_mode='bootstrap3')
+admin.add_view(AdminView(User, db.session))
+admin.add_view(AdminView(Role, db.session))
+
+
+app.vacant_spot_count = 0
+
+# Executes before the first request is processed.
+#TODO check expiration
+@app.before_first_request
+def before_first_request():
+    db.create_all()
+    # rpi = User(username='rpi', email='rpi@rpi.edu')
+    # rpi.set_password('password')
+    # rpi.roles.append(Role(name='RPi'))
+    # rpi.roles.append(Role(name='Admin'))
+    # db.session.add(rpi)
+    # db.session.commit()
+
+@app.before_request
+def before_request():
+    reserved_users = User.query.filter_by(reserved=True).all()
+    for user in reserved_users:
+        difference = datetime.datetime.utcnow() - datetime.datetime.strptime(str(user.timestamp),'%Y-%m-%d %H:%M:%S.%f')
+        if difference.total_seconds() > 600:
+            user.reserved = False
+            user.code = -1
+            db.session.commit()
--- App/app/auth/routes.py
+++ App/app/auth/routes.py
@@ -6,7 +6,6 @@ from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
 
-
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if current_user.is_authenticated:
